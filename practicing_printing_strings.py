777fac363827f2c3089cacbf06125c2912cf5c05
Acee Zai Mendez <zaiakari7@gmail.com>
2026-03-08 18:48:23 +0800

    Added two new programs.

--- practicing_printing_strings.py
+++ practicing_printing_strings.py
@@ -3,3 +3,9 @@ first_number = float(input("Enter first number: "))
 second_number = float(input("Enter second number: "))
 print(min(first_number, second_number))
 
+# Prog02: Inequality Check
+-
+# Prog03: Simple Subtraction
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
+print(first_number - second_number)
